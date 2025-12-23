c417321f0a1f31d2a63f526edbcd26a89ed777bd
George Steel <george@net-glue.co.uk>
2025-12-23 22:09:06 +0000

    Add explanatory notes to the `CurrencySymbolStyle` enum
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Helper/CurrencySymbolStyle.php
+++ src/Helper/CurrencySymbolStyle.php
@@ -6,6 +6,14 @@ namespace Laminas\I18n\View\Helper;
 
 use function preg_replace;
 
+/**
+ * An enum to simplify currency symbol usage in intl message formats for currency
+ *
+ * The intl extension (ICU) uses the '¤' symbol to represent any currency symbol in a formatting pattern such as this
+ * example in `en_GB`: "¤#,##0.00" would yield "£1,234.56", whereas "¤¤#,##0.00" would yield "GBP 1,234.56"
+ *
+ * @link https://unicode.org/reports/tr35/tr35-numbers.html#Number_Pattern_Character_Definitions
+ */
 enum CurrencySymbolStyle: string
 {
     case None     = 'none';
